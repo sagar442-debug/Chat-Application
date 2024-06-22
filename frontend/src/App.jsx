45a0c8ecf599f53334612f0e727a7f6535582904
sagar442-debug <bnradlyp1@gmail.com>
2024-06-22 15:06:28 -0600

    Created frontend UI ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div className="p-4 h-screen flex items-center justify-center font-montserrat">
-      <SignUp />
+      <Home />
     </div>
   );
 };
