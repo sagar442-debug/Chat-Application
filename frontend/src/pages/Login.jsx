d091fe050a52be2a51cae35755dea58cb323e56f
sagar442-debug <bnradlyp1@gmail.com>
2024-06-23 07:19:46 -0600

    Created sign in functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../hooks/useLogin";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { loading, login } = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-sky-950 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0">
@@ -8,7 +18,7 @@ const Login = () => {
           Login
           <span className="text-blue-500"> ChatApp</span>
         </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text text-gray-300">
@@ -19,6 +29,8 @@ const Login = () => {
               type="text"
               placeholder="Enter username"
               className="w-full input input-bordered h-10"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div>
@@ -31,6 +43,8 @@ const Login = () => {
               type="password"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <Link
@@ -40,8 +54,15 @@ const Login = () => {
             {"Don't"} have an account?{" "}
           </Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2 hover:text-gray-100">
-              Login
+            <button
+              className="btn btn-block btn-sm mt-2 hover:text-gray-100"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Login"
+              )}
             </button>
           </div>
         </form>
