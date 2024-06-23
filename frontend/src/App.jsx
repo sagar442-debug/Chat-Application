8bfee30bc1877de17e3c19bc2401659d20bb8d93
sagar442-debug <bnradlyp1@gmail.com>
2024-06-23 06:34:52 -0600

    Added the sign up functionality with protected routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 const App = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center font-montserrat">
       <Routes>
-        <Route path={"/"} element={<Home />} />
-        <Route path={"/login"} element={<Login />} />
-        <Route path={"/signup"} element={<SignUp />} />
+        <Route
+          path={"/"}
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path={"/login"}
+          element={authUser ? <Navigate to={"/"} /> : <Login />}
+        />
+        <Route
+          path={"/signup"}
+          element={authUser ? <Navigate to={"/"} /> : <SignUp />}
+        />
       </Routes>
       <Toaster />
     </div>
